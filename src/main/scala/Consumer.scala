5df1c93ec2707fe57c8295cb6546432e71537abe
pavelrzn <0p9o8i7u6y>
2021-09-13 23:29:33 +0300

    Initial

--- src/main/scala/Consumer.scala
+++ src/main/scala/Consumer.scala
@@ -1,24 +1,39 @@
-import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
+import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.serialization.StringDeserializer
-import scala.jdk.CollectionConverters._
 
-import java.util.Properties
+import scala.jdk.CollectionConverters.{CollectionHasAsScala, IterableHasAsScala, SeqHasAsJava}
+import java.util.{Collections, Properties}
 import java.time.Duration
 
 object Consumer extends App {
 
   val props = new Properties()
-  props.put("bootstrap.servers", "localhost:29092")
-  props.put("group.id", "consumer1")
+  props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092")
+  props.put(ConsumerConfig.GROUP_ID_CONFIG, "homework")
 
   val consumer = new KafkaConsumer(props, new StringDeserializer, new StringDeserializer)
+  consumer.subscribe(Collections.singletonList("books"))
 
-  consumer.subscribe(List("mytopic").asJavaCollection)
-
-  consumer
-    .poll(Duration.ofSeconds(1))
+  val partitions = consumer.partitionsFor("books")
     .asScala
-    .foreach { r => println(r.value()) }
+    .map(x=> new TopicPartition(x.topic(), x.partition()))
+    .toList.asJava
+
+  val endOffsets = consumer.endOffsets(partitions)
+
+  consumer.unsubscribe()
+  consumer.assign(partitions)
+  partitions.forEach(part => {
+    consumer.seek(part, endOffsets.get(part) - 5)
+  })
+
+  val data = consumer
+    .poll(Duration.ofSeconds(10))
+    .asScala
+
+  println(s"data size = ${data.size}")
+  data.foreach(println(_))
 
   consumer.close()
 
--- src/main/scala/Producer.scala
+++ src/main/scala/Producer.scala
@@ -1,24 +1,61 @@
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringSerializer
+import org.json4s.jackson.Serialization
+import org.json4s.{DefaultFormats, Formats}
 
+import java.io.{File, FileReader}
 import java.util.Properties
 
 object Producer extends App {
+
+  case class BestsellersWithCategories(
+                                        Name: String,
+                                        Author: String,
+                                        `User Rating`: String,
+                                        Reviews: String,
+                                        Price: Double,
+                                        Year: String,
+                                        Genre: String
+                                      )
 
   val props = new Properties()
   props.put("bootstrap.servers", "localhost:29092")
 
   val producer = new KafkaProducer(props, new StringSerializer, new StringSerializer)
 
-  val messages = List(
-    "message1",
-    "message2",
-    "message3",
-  )
+  import org.apache.commons.csv.CSVFormat
+
+  implicit val formats: Formats = DefaultFormats
+
+  val file = new File(getClass.getResource("bestsellers_with_categories.csv").getPath)
+  val in = new FileReader(file)
+
+  import scala.jdk.CollectionConverters.IteratorHasAsScala
+
+  val records = CSVFormat
+    .RFC4180
+    .withHeader("Name", "Author", "User Rating", "Reviews", "Price", "Year", "Genre")
+    .withSkipHeaderRecord()
+    .parse(in)
+    .iterator()
+    .asScala
+    .toList
+
+  in.close()
+
+  val messages = records.map(record => Serialization.write(BestsellersWithCategories(
+    record.get("Name"),
+    record.get("Author"),
+    record.get("User Rating"),
+    record.get("Reviews"),
+    record.get("Price").toDouble,
+    record.get("Year"),
+    record.get("Genre")
+  )))
 
   messages.foreach { m =>
-    producer.send(new ProducerRecord("mytopic", m, m))
+    producer.send(new ProducerRecord("books", m))
   }
 
   producer.close()
